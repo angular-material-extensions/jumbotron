dde175cd529896b3ff97fb8962e9b86bf6b40e5f
Anthony Nahas <anthony.na@hotmail.de>
2018-06-05 00:27:54 +0200

    demo(app): integration of mat-jumbotron component in the demo app

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {  LibModule  } from '@angular-material-extensions/jumbotron';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatJumbotronModule} from '@angular-material-extensions/jumbotron';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomeComponent} from './home.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        LibModule.forRoot(),
-        HomeRoutingModule,
-    ],
-    declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    MatJumbotronModule.forRoot(),
+    HomeRoutingModule,
+  ],
+  declarations: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {
+}

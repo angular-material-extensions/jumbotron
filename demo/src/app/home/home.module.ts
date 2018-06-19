a7e9b92b0f2271813f9da2732fb0481d4483d70d
Anthony Nahas <anthony.na@hotmail.de>
2018-06-19 20:17:41 +0200

    demo(app): integration of `mat-jumbotron` component

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -4,12 +4,17 @@ import {MatJumbotronModule} from '@angular-material-extensions/jumbotron';
 
 import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './home.component';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
     MatJumbotronModule.forRoot(),
     HomeRoutingModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   declarations: [HomeComponent],
 })
